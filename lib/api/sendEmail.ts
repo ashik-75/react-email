c6096ae43e81f6be256cda52eca635a8326139ab
Ashik <ashik@pop-os.localdomain>
2023-06-10 23:58:52 +0600

    change url

--- lib/api/sendEmail.ts
+++ lib/api/sendEmail.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 export async function sendRequestToBack(payload: any) {
-  const response = await axios.post("http://localhost:3000/api/email", payload);
+  const url =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/api/email"
+      : "https://react-email-c54f.vercel.app/api/email";
+  const response = await axios.post(url, payload);
 
   return response.data;
 }
