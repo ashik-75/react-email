b77da922732e33e0d256fb07f8b69833d6a47bce
Ashik <ashik@pop-os.localdomain>
2023-06-10 22:42:15 +0600

    edge runtime

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -1,7 +1,10 @@
 import Email from "@/components/Email";
 import { NextRequest, NextResponse } from "next/server";
-
 import { Resend } from "resend";
+
+export const config = {
+  runtime: "edge", // for Edge API Routes only
+};
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
