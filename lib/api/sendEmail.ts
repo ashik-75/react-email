aad4a4dbd7e250272a59e457f5c04163af1b5078
Ashik <ashik@pop-os.localdomain>
2023-06-11 00:14:24 +0600

    push data

--- lib/api/sendEmail.ts
+++ lib/api/sendEmail.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 export async function sendRequestToBack(payload: any) {
-  const url =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/api/email"
-      : "https://react-email-c54f.vercel.app/api/email";
+  const url = "https://react-email-c54f.vercel.app/api/email";
   const response = await axios.post(url, payload);
 
   return response.data;
